38c0cc5b8a0ce33f559ad1827b22a7d0458fa976
kjh <kimjh4495@gmail.com>
2023-06-07 16:06:38 +0900

    first commit

---
+++ lib/main_page.dart
@@ -0,0 +1,100 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_kmdb/movie_api.dart';
+import 'movie_Form.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({
+    super.key,
+  });
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late var controller = TextEditingController();
+  dynamic body = Center(
+    child: Text('!Select Movies!', style: TextStyle(fontSize: 20)),
+  );
+
+  void searchMovie(String keyword) async {
+    MovieApi movieApi = MovieApi();
+    var movies = movieApi.search(keyword);
+
+    setState(() {
+      body = FutureBuilder(
+        future: movies,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var movieData = snapshot.data;
+            return ListView.separated(
+                itemBuilder: (context, index) {
+                  return MovieForm(movie: movieData[index]);
+                },
+                separatorBuilder: (context, index) => Divider(),
+                itemCount: movieData!.length);
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
+      );
+      // body = ListView.separated(
+      //     itemBuilder: (context, index) {
+      //       return MovieForm(movie: movies[index]);
+      //     },
+      //     separatorBuilder: (context, index) => Divider(),
+      //     itemCount: movies.length);
+    });
+  }
+
+  void showSearchPage() async {
+    var result = showModalBottomSheet(
+      backgroundColor: Colors.transparent,
+      context: context,
+      builder: (context) => Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10), topRight: Radius.circular(10))),
+        height: 200,
+        child: Column(
+          children: [
+            ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.black)),
+                icon: Icon(Icons.close),
+                label: Text('닫기')),
+            TextFormField(
+              controller: controller,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  searchMovie(controller.text);
+                  Navigator.pop(context);
+                },
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.black)),
+                child: Text('검색하기'))
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: body,
+      floatingActionButton: FloatingActionButton(
+        onPressed: showSearchPage,
+        child: Icon(Icons.search),
+      ),
+    );
+  }
+}
---
+++ lib/movie_api.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+//http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=9533HA48052PTJ8HVLZV&query=인어공주&detail=Y
+
+class MovieApi {
+  final api_key = 'LN636A57WG804S1Y7ZF8';
+
+  Future<List<dynamic>> search(String keyword) async {
+    String site =
+        'http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=$api_key&query=$keyword&detail=Y';
+    var response = await http.get(Uri.parse(site));
+    if (response.statusCode == 200) {
+      var movies =
+          jsonDecode(response.body)['Data'][0]['Result'] as List<dynamic>;
+      return movies;
+    } else {
+      return [];
+    }
+  }
+}
