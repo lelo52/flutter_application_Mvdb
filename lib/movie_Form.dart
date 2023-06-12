7f0708521706db7398425997e8d5e6691c847fb7
leelo52 <115298517+lelo52@users.noreply.github.com>
2023-06-13 08:17:45 +0900

    Update movie_Form.dart

--- lib/movie_Form.dart
+++ lib/movie_Form.dart
@@ -1,53 +1,81 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_kmdb/movie_detail.dart';
+import 'kobis_api.dart';
 
-class MovieForm extends StatelessWidget {
-  Map<String, dynamic> movie; // movie['nation']
+void main() {
+  runApp(MyApp());
+}
 
-  MovieForm({super.key, required this.movie});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    var directorNm = movie['directors']['director'][0]['directorNm'];
-    var actorNm = movie['actors']['actor'][0]['actorNm'];
-    var posters = movie['posters'].split('|')[0];
-    var movieTitle = movie['title']
-        .toString()
-        .replaceAll(' !HS ', '')
-        .replaceAll(' !HE ', '');
+  _MyAppState createState() => _MyAppState();
+}
 
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => MovieDetail(movie: movie),
-            ));
-      },
-      child: Container(
-        height: 150,
-        padding: EdgeInsets.all(10),
-        child: Row(children: [
-          SizedBox(
-            child: Hero(tag: movie['movieSeq'], child: Image.network(posters)),
-            width: 80,
-            height: 100,
-          ),
-          Expanded(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '$movieTitle(${movie['prodYear']})',
-                style: TextStyle(fontSize: 20),
-              ),
-              Text('$directorNm/$actorNm'),
-              Text('${movie['repRlsDate']}'),
-              Text('${movie['audiAcc']}'),
-              Text('${movie['rating']}'),
-            ],
-          ))
-        ]),
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+class _MainPageState extends State<MainPage> {
+  dynamic mvList = const Text("검색하세요");
+
+  void showCal() async {
+    var dt = await showDatePicker(
+      context: context,
+      initialDate: DateTime(2010, 1, 1),
+      firstDate: DateTime(2010, 1, 1),
+      lastDate: DateTime.now(),
+    );
+    if (dt != null) {
+      String tp = dt.toString().split(' ')[0].replaceAll('-', '');
+      var koApi = Api();
+      var komv = await koApi.search(tp: tp);
+
+      setState(() {
+        if (komv.isEmpty) {
+          mvList = Center(
+            child: Text('결과가 텅텅'),
+          );
+        } else {
+          mvList = ListView.separated(
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(komv[index]['rank']+"위 "+komv[index]['movieNm']),
+                subtitle: Text(komv[index]['openDt']
+                    .toString()
+                    .replaceAll('<br/>', '\n')),
+              );
+            },
+            separatorBuilder: (context, index) => Divider(),
+            itemCount: komv.length,
+          );
+        }
+      });
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("20230101"),
+          Expanded(child: mvList),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: showCal,
+        child: Icon(Icons.calendar_today),
       ),
     );
   }
